d9a1991e51110d1fc5648ec4f63213fe1080b834
Matt Pahuta <mhpahuta@gmail.com>
2024-05-23 16:11:43 -0700

    Add initial about section, styles, markup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,7 @@ export default {
 				cyanLight: 'hsl(180, 56%, 75%)',
 				darkViolet: 'hsl(257, 27%, 26%)',
 				red: 'hsl(0, 87%, 67%)',
+				lightGray: 'hsl(225, 32%, 95%)',
 				gray: 'hsl(0, 0%, 75%)',
 				grayishViolet: 'hsl(257, 7%, 63%)',
 				veryDarkBlue: 'hsl(255, 11%, 22%)',
